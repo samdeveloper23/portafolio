9c530b3182865ccb406ac0ce0d4630442165b129
samdeveloper23 <sam.developer.23@gmail.com>
2023-11-08 10:40:41 +0100

    pruebaUno

---
+++ src/screens/Frame/Frame.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <img className="samperfil-removebg" alt="Samperfil removebg" src="/img/samperfil-removebg-preview-1.png" />
+            <p className="text-wrapper">Sam Machado - Develop Full Start</p>
+            <img className="codificacion" alt="Codificacion" src="/img/codificacion-2.png" />
+          </div>
+          <img className="img" alt="Codificacion" src="/img/codificacion-2.png" />
+        </div>
+        <div className="overlap-group-2">
+          <img className="rectangle" alt="Rectangle" src="/img/rectangle-1.png" />
+          <div className="rectangle-2" />
+          <img
+            className="react-javascript-js"
+            alt="React javascript js"
+            src="/img/react-javascript-js-framework-facebook-svgrepo-com-1.svg"
+          />
+          <img className="jquery-original" alt="Jquery original" src="/img/jquery-original-wordmark-1.svg" />
+          <img className="angular-seeklogo-com" alt="Angular seeklogo com" src="/img/angular-seeklogo-com-1.svg" />
+          <img className="nodejs-seeklogo-com" alt="Nodejs seeklogo com" src="/img/nodejs-seeklogo-com-1.svg" />
+          <img className="npm-node-package" alt="Npm node package" src="/img/npm-node-package-manager-1.svg" />
+          <img className="socket-telecom" alt="Socket telecom" src="/img/socket-telecom-seeklogo-com-1.svg" />
+          <img className="mysql-seeklogo-com" alt="Mysql seeklogo com" src="/img/mysql-seeklogo-com-1.svg" />
+          <img className="mariadb-seeklogo-com" alt="Mariadb seeklogo com" src="/img/mariadb-seeklogo-com-1.svg" />
+          <img className="github-seeklogo-com" alt="Github seeklogo com" src="/img/github-seeklogo-com-1.svg" />
+          <img className="gitlab-seeklogo-com" alt="Gitlab seeklogo com" src="/img/gitlab-seeklogo-com-1.svg" />
+          <img className="git-seeklogo-com" alt="Git seeklogo com" src="/img/git-seeklogo-com-1.svg" />
+          <img className="with-wordmark" alt="With wordmark" src="/img/html5-with-wordmark-color-1.svg" />
+          <img className="css-seeklogo-com" alt="Css seeklogo com" src="/img/css-3-seeklogo-com-1.svg" />
+        </div>
+        <div className="proyectos-https">
+          Proyectos:
+          <br />
+          <br />
+          https://cerulean-banoffee-c3a7c0.netlify.app/proyectos
+        </div>
+        <div className="overlap-2">
+          <div className="rectangle-3" />
+          <div className="titulos-https">
+            Titulos:
+            <br />
+            <br />
+            https://cerulean-banoffee-c3a7c0.netlify.app/titulos
+          </div>
+          <div className="rectangle-4" />
+          <div className="text-wrapper-2">Descripción:</div>
+          <div className="rectangle-5" />
+          <div className="text-wrapper-3">Empresa:</div>
+          <div className="text-wrapper-4">Nombre:</div>
+          <div className="rectangle-6" />
+          <div className="rectangle-7" />
+          <div className="text-wrapper-5">Email:</div>
+          <div className="rectangle-8" />
+        </div>
+      </div>
+    </div>
+  );
+};
