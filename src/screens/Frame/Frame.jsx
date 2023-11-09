d844b30f1b5f894f193e84664d3f970299f8cffe
samdeveloper23 <sam.developer.23@gmail.com>
2023-11-09 10:17:32 +0100

    portafolios terminado v1.0

--- src/screens/Frame/Frame.jsx
+++ src/screens/Frame/Frame.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./style.css";
-import { ColorControl } from "@storybook/blocks";
 
 export const Frame = () => {
   return (
     <div className="frame">
       <div className="div">
+        <img className="samperfil-removebg" alt="Samperfil removebg" src="/img/samperfil-removebg-preview-1.png" />
         <div className="overlap">
-          <div className="overlap-group">
-            <img className="samperfil-removebg" alt="Samperfil removebg" src="/img/samperfil-removebg-preview-1.png" />
-            <p className="text-wrapper">Sam Machado Developer Full Start</p>
-            <img className="codificacion" alt="Codificacion" src="/img/codificacion-2.png" />
-          </div>
-          <img className="img" alt="Codificacion" src="/img/codificacion-2.png" />
+          <p className="text-wrapper">-Ismael Machado- Developer Full Start</p>
+          <img className="codificacion" alt="Codificacion" src="/img/codificacion-2.png" />
         </div>
-        <div className="overlap-group-2">
+        <div className="overlap-group">
           <img className="rectangle" alt="Rectangle" src="/img/rectangle-1.png" />
           <div className="rectangle-2" />
           <img
@@ -35,29 +31,48 @@ export const Frame = () => {
           <img className="with-wordmark" alt="With wordmark" src="/img/html5-with-wordmark-color-1.svg" />
           <img className="css-seeklogo-com" alt="Css seeklogo com" src="/img/css-3-seeklogo-com-1.svg" />
         </div>
-        <div className="proyectos-https">
-          Proyectos:
-          <br />
-          <br />
-          <p className="red">en construcción</p>
-        </div>
+        <img className="img" alt="Codificacion" src="/img/codificacion-2.png" />
+        <div className="text-wrapper-2">Projects:</div>
         <div className="overlap-2">
-          <div className="rectangle-3" />
-          <div className="titulos-https">
-            Titulos:
-            <br />
-            <br />
-            <p className="red">en construcción</p>
+          <div className="text-wrapper-3">Titles:</div>
+          <img className="captura-desde" alt="Captura desde" src="/img/captura-desde-2023-11-09-08-44-01-1.png" />
+          <img className="captura-desde-2" alt="Captura desde" src="/img/captura-desde-2023-11-09-08-41-07-1.png" />
+          <img className="captura-desde-3" alt="Captura desde" src="/img/captura-desde-2023-11-09-08-40-11-1.png" />
+        </div>
+        <div className="overlap-3">
+          <div className="overlap-4">
+            <div className="text-wrapper-4">Description:</div>
+            <input type="text" className="rectangle-3" />
           </div>
-          <div className="rectangle-4" />
-          <div className="text-wrapper-2">Descripción:</div>
-          <textarea type="text" className="rectangle-5" />
-          <div className="text-wrapper-3">Empresa:</div>
-          <div className="text-wrapper-4">Nombre:</div>
-          <input type="text" className="rectangle-6" />
-          <input type="text" className="rectangle-7" />
-          <div className="text-wrapper-5">Email:</div>
-          <input type="text" className="rectangle-8" />
+          <div className="overlap-5">
+            <div className="text-wrapper-5">Company:</div>
+            <input type="text" className="rectangle-4" />
+          </div>
+          <div className="overlap-6">
+            <div className="text-wrapper-6">Name:</div>
+            <input type="text" className="rectangle-4" />
+          </div>
+          <div className="overlap-7">
+            <div className="text-wrapper-5">Email:</div>
+            <input type="text" className="rectangle-5" />
+          </div>
+          <div className="overlap-8">
+            <div className="rectangle-6" />
+            <img className="plane-svgrepo-com" alt="Plane svgrepo com" src="/img/plane-svgrepo-com-1.svg" />
+          </div>
+        </div>
+        <a href="https://courageous-mandazi-4cff23.netlify.app/" target="_blank"><img className="captura-desde-4" alt="Captura desde" src="/img/captura-desde-2023-11-09-07-25-36-1.png" /></a>
+        <a href="https://willowy-kelpie-d8598b.netlify.app/" target="_blank"><img className="captura-desde-5" alt="Captura desde" src="/img/captura-desde-2023-11-09-07-26-37-1.png" /></a>
+        <a href="https://moonlit-sawine-5d687d.netlify.app/" target="_blank"><img className="captura-desde-6" alt="Captura desde" src="/img/captura-desde-2023-11-09-07-26-53-1.png" /></a>
+        <a href="https://dancing-beignet-23847c.netlify.app/" target="_blank"><img className="captura-desde-7" alt="Captura desde" src="/img/captura-desde-2023-11-09-07-34-01-1.png" /></a>
+        <a href="https://6499c8e6f981b704028e9a61--cozy-kringle-f088d0.netlify.app/" target="_blank"><img className="captura-desde-8" alt="Captura desde" src="/img/captura-desde-2023-11-09-07-34-37-1.png" /></a>
+        <div className="text-wrapper-7">Contact:</div>
+        <a href="https://www.linkedin.com/in/samael-machado-919a19274/" target="_blank"><img className="linkedin-svgrepo" alt="Linkedin svgrepo" src="/img/linkedin-1-svgrepo-com-1.svg" /></a>
+        <a href="https://github.com/samdeveloper23" target="_blank"><img className="github-svgrepo-com" alt="Github svgrepo com" src="/img/github-svgrepo-com-1.svg" /></a>
+        <div className="mvl-email">
+          mvl: 654256127
+          <br />
+          email: sam.developer.23@gmail.com
         </div>
       </div>
     </div>
